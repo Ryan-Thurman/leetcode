dcd287181e5490a9c2e0403046b95414902dadf7
Ryan <postman_mc123@yahoo.com>
2022-01-25 14:54:08 -0800

    solutions for #11 & #15

---
+++ 15-3sum.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const result = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    //skip if we have already used the number at the start of the loop
+    if (i > 0 && nums[i] == nums[i - 1]) {
+      continue;
+    }
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+
+        while (nums[left] === nums[left - 1] && left < right) {
+          left++;
+        }
+      }
+    }
+  }
+
+  return result;
+};
